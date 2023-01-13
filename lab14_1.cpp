8d947baa184e9300446d305ce13a1939c41529a5
Phurinon <folk.5834@gmail.com>
2023-01-13 17:28:43 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,37 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum;
+    for(int i=0;i<N;i++){
+        sum += A[i];
+        B[0] = sum/N;
+    }
+
+    double standard;
+    for(int i=0;i<N;i++){
+        standard += A[i]*A[i];
+        B[1] = sqrt(standard/N-pow(sum/N,2));
+    }
+
+    double Geo;
+    float product = 1;
+    for(int i=0;i<N;i++){
+        product = product*A[i];
+    }
+    Geo = pow(product,(float)1/N);
+    B[2] = Geo;
+
+    double Har=0;
+    for(int i=0;i<N;i++){
+        Har += (float)1/A[i];
+        B[3] = (float)N/Har;
+    }
+
+    double max=A[0],min=A[0];
+    for(int i=1;i<N;i++){
+        if(A[i]>max) B[4]=A[i];
+        if(A[i]<min) B[5]=A[i];
+    }
+}
